cf8fef798938586c88dce549984f5cefd18e15a0
thaaanks <alleswirdbesser3@gmail.com>
2020-12-14 00:44:16 +0100

    Update 201206_Adv.js

--- 201206_Adv.js
+++ 201206_Adv.js
@@ -10,21 +10,40 @@ the output should be sortByLength(inputArray) = ["", "a", "zz","abc","aaa"]
 Hints> sort()
 */
 
+// ⭐오늘 배운 것 ==================================
 
+// =================================================
+
+// 내 답안 (인터넷 참조..)
 function sortByLength(inputArray){
-    for(let i=0; i<inputArray.length; i++){
-        let elementLengthArr = [];
-        elementLengthArr.push(inputArray[i].length);
-        console.log(elementLengthArr);
-        inputArray[i].concat([i]);
-        console.log(inputArray);
+    let sorted = inputArray.sort(function(a,b){
+         return a.length - b.length;
+     });
+     return sorted;
+ }
+ sortByLength(["abc", "", "aaa","a","zz"]);
+ 
 
+// 출제자 답안 (아직 안봄)
+
+
+// 인터넷 검색전 고군분투 흔적들...
+function sortByLength(inputArray){
+    let elementLengthArr = [];
+    let i = 0;
+   //  let arrIndex = inputArray[i]; 
+    for(let i=0; i<inputArray.length; i++){   
+        let arrIndex = inputArray[i]; 
+          console.log(arrIndex);
+        elementLengthArr.push(arrIndex.length);
+          console.log(elementLengthArr);
     }
     elementLengthArr.sort(function(a,b){
-        return a-b;
-    });
-    cons
+       return a-b;
+    })
+    console.log(elementLengthArr);
 }
+sortByLength(["abc", "", "aaa","a","zz"]);
 
 
 
