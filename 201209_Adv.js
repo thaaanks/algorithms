a9ebf0761fc39527b9a2d53ade7eb576a454fe32
thaaanks <alleswirdbesser3@gmail.com>
2020-12-17 23:42:01 +0100

    Create 201209_Adv.js

---
+++ 201209_Adv.js
@@ -0,0 +1,28 @@
+* Day 9
+Given a positive integer Number, 
+return the sum of all odd Fibonacci numbers 
+that are less than or equal to num. 
+
+The first two numbers in the Fibonacci sequence are 1 and 1.
+Every additional number in the sequence is the sum of the two previous numbers.
+The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3 and 5.
+
+Example>
+sumOddFibonacciNums(10) should return 10
+sumOddFibonacciNums(1000) should return 1785
+sumOddFibonacciNums(4000000) should return 4613732
+
+
+function sumOddFibonacciNums(int){
+    let fibiArray = [];
+    for(let i = 0; i< ; i++){
+        let n0 = 1;
+        let n1 = 1;
+        let n2 = n0 + n1;
+        let n3 = n1 + n2;
+        let n4 = n2 + n3;
+
+    }
+}
