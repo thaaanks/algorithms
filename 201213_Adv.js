5ddb661f1d830ed092c4535f3cd7dcf5d89c0e52
thaaanks <alleswirdbesser3@gmail.com>
2020-12-24 23:55:58 +0100

    Create 201213_Adv.js

---
+++ 201213_Adv.js
@@ -0,0 +1,19 @@
+Given array of integers, remove each kth element from it.
+
+Example>
+For inputArray = [1,2,3,4,5,6,7,8,9,10] and k = 3, 
+the output should be extractEachKth(inputArray,k)  = [1,2,4,5,7,8,10].
+
+Hints>
+filter()
+
+// 내 답안
+extractEachKth(inputArray,k){
+    let removedEl = [];
+    for (i=1; ki<=inputArray.length; i++){
+        removedEl.push(ki);
+    }
+    let answer = inputArray - removedEl;
+    return answer;
+}
+
