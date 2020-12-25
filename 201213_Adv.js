01e97cf29bb973a1665099431f26895983281807
thaaanks <alleswirdbesser3@gmail.com>
2020-12-25 01:09:53 +0100

    extractEachKth( ), filter( )

--- 201213_Adv.js
+++ 201213_Adv.js
@@ -7,13 +7,22 @@ the output should be extractEachKth(inputArray,k)  = [1,2,4,5,7,8,10].
 Hints>
 filter()
 
-// 내 답안
-extractEachKth(inputArray,k){
-    let removedEl = [];
-    for (i=1; ki<=inputArray.length; i++){
-        removedEl.push(ki);
-    }
-    let answer = inputArray - removedEl;
-    return answer;
-}
+// 내 답안1
+function extractEachKth(inputArray,k){
+    let max = inputArray.length;
+    for (i=1; k*i<=max; i++){
+       let idx = inputArray.indexOf(k*i);
+       inputArray.splice(idx,1);
+        console.log(inputArray);
+     }
+     return inputArray;
+ } 
 
+ let inputArray = [1,2,3,4,5,6,7,8,9,10];
+ let k = 3;
+ extractEachKth(inputArray,k)
+ 
+// 출제자 답안
+function extractEachKth(nums, index) {
+    return nums.filter((value, i) => (i + 1) % index !== 0);
+}
