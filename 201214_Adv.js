f2cb3748dd58c1967b30597982c398fb031fd2b1
thaaanks <alleswirdbesser3@gmail.com>
2020-12-25 23:45:17 +0100

    Create 201214_Adv.js

---
+++ 201214_Adv.js
@@ -0,0 +1,14 @@
+* Day 14
+Given an array of integers, 
+find the maximal absolute difference between any two of its adjacent customElements.
+
+Example>
+for inputArray = [2,4,1,0], the output should be arrayMaximalAdjacentDifference(inputArray) = 3. 
+for inputArray = [2,9,1,0], the output should be arrayMaximalAdjacentDifference(inputArray) = 8. 
+
+Hints>
+Math.abs()
+*/
+
+
+
