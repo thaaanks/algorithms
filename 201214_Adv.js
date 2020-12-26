d64a30e22309e544a05f7347b7da5c5aec39776d
thaaanks <alleswirdbesser3@gmail.com>
2020-12-26 23:35:19 +0100

    Update 201214_Adv.js

--- 201214_Adv.js
+++ 201214_Adv.js
@@ -10,5 +10,15 @@ Hints>
 Math.abs()
 */
 
+// 내 답안, 왜 문제답에서 0은 제외하고 계산하지??
+function arrayMaximalAdjacentDifference(inputArray){
+    let max = Math.max.apply(null, inputArray);
+    let min = Math.min.apply(null, inputArray);
+    let answer - max - min;
+    return answer;
+}
+
+let inputArray = [2,4,1,0];
+arrayMaximalAdjacentDifference(inputArray)
 
 
